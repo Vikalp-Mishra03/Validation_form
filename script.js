ba9769b0420df3ad3671979cb0c3d1673f7bce06
Vikalp Mishra <mishravikalp896@gmail.com>
2023-07-25 12:35:35 +0530

    name section complete

---
+++ script.js
@@ -0,0 +1,21 @@
+var nameError = document.getElementById("name-error")
+var phoneError = document.getElementById("phone-error")
+var emailError = document.getElementById("email-error")
+var messageError = document.getElementById("message-error")
+var submitError = document.getElementById("submit-error")
+
+function ValidateName() {
+    var name = document.getElementById("contact-name").value;
+
+    if (name.length == 0) {
+        nameError.innerHTML = "Name is required"
+        return false
+    }
+    if (!name.match(/^[A-Za-z]+\s[A-Za-z]+$/)) {
+        nameError.innerHTML = "Write full name";
+        return false;
+    }
+    
+    nameError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    return true
+}
