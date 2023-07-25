69808209f74723fa1e052c4fb7dbd5ed107b435f
Vikalp Mishra <mishravikalp896@gmail.com>
2023-07-25 12:42:46 +0530

    phone section logic complete

--- script.js
+++ script.js
@@ -19,3 +19,22 @@ function ValidateName() {
     nameError.innerHTML = '<i class="fas fa-check-circle"></i>';
     return true
 }
+function ValidatePhone() {
+    var phone = document.getElementById("contact-phone").value;
+
+    if (phone.length == 0) {
+        phoneError.innerHTML = "Phone no. is required"
+        return false
+    }
+    if (phone.length !== 10) {
+        phoneError.innerHTML = "Phone no. should be 10 digits"
+        return false
+    }
+    if (!phone.match(/^[0-9]{10}$/)) {
+        phoneError.innerHTML = "Only digits please";
+        return false;
+    }
+    
+    phoneError.innerHTML = '<i class="fas fa-check-circle"></i>';
+    return true
+}
